7691c0f55c8f26216eb026de7f0a9f447ce1594c
Stanislav Elenin <stanislaveleninweb@gmail.com>
2020-04-19 21:58:37 +0300

    Create PollObject Model --all

---
+++ database/factories/PollObjectFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\PollObject;
+use Faker\Generator as Faker;
+
+$factory->define(PollObject::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
