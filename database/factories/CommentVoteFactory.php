2a7633dee56513e2cfbd567bb93c2853dc5f1840
Stanislav Elenin <stanislaveleninweb@gmail.com>
2020-04-19 21:21:41 +0300

    Create CommentVote Model --all

---
+++ database/factories/CommentVoteFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\CommentVote;
+use Faker\Generator as Faker;
+
+$factory->define(CommentVote::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
