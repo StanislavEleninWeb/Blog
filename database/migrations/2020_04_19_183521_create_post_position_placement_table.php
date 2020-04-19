40b9d2b73b44a78847c59aff36954c177ad9f6af
Stanislav Elenin <stanislaveleninweb@gmail.com>
2020-04-19 21:38:58 +0300

    create_post_position_placement_table

---
+++ database/migrations/2020_04_19_183521_create_post_position_placement_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostPositionPlacementTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_position_placement', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_position_placement');
+    }
+}
