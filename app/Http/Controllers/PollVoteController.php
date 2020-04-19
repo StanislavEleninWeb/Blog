51e2e53373bdbf46f3774132ee570916e46de953
Stanislav Elenin <stanislaveleninweb@gmail.com>
2020-04-19 22:05:40 +0300

    Create PollVote Model --all

---
+++ app/Http/Controllers/PollVoteController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\PollVote;
+use Illuminate\Http\Request;
+
+class PollVoteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\PollVote  $pollVote
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PollVote $pollVote)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\PollVote  $pollVote
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PollVote $pollVote)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\PollVote  $pollVote
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PollVote $pollVote)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PollVote  $pollVote
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PollVote $pollVote)
+    {
+        //
+    }
+}
---
+++ database/factories/PollVoteFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\PollVote;
+use Faker\Generator as Faker;
+
+$factory->define(PollVote::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
