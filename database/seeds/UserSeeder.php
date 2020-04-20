f289f982969c9c59c4c00f6aee72cd61f205548b
StanislavEleninWeb <stanislaveleninweb@gmail.com>
2020-04-20 14:27:53 +0300

    Create UserSeeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Stanislav Elenin',
+            'email' => 'stanislaveleninweb@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'active' => 1,
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        
+        factory(App\User::class, 10)->create();
+    }
+}
