c25abec755983400169525e20be7c5ac1f7d2c75
StanislavEleninWeb <stanislaveleninweb@gmail.com>
2020-04-20 14:37:04 +0300

    Fill TagSeeder

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -7,6 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Tag::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->word(),
+        'slug' => $faker->slug(1),
     ];
 });
