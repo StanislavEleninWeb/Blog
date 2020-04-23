0e3e790c9e0ad1fbdda64408c157d27d4f649579
Stanislav Elenin <stanislaveleninweb@gmail.com>
2020-04-24 00:17:48 +0300

    Template

--- resources/views/home/admin/index.blade.php
+++ resources/views/home/admin/index.blade.php
@@ -1,1 +1,13 @@
 @extends('layouts.app')
+
+@section('content')
+
+<x-alerts.alert type="danger" message="Alert message" class="mt-4">
+Component slot
+</x-alerts.alert>
+
+<x-inputs.buttons.back class="btn-lg" type="secondary" text="Back" url="http://blog.stanislavelenin.demo"/>
+
+<x-inputs.buttons.submit type="primary" text="Submit" class="btn-lg mt-4"/>
+
+@endsection
