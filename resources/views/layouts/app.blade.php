1eb79b9187cc6dd723eed12e534b0ee3d0b6d12c
Stanislav Elenin <stanislaveleninweb@gmail.com>
2020-04-23 22:40:29 +0300

    Layout template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,15 +9,19 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    @section('javascript')
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    @show
 
+    @section('css')
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @show
 </head>
 <body>
     <div id="app">
