1d1d92b86179d6b1a12c6843ac2508ec01dfa585
StanislavEleninWeb <stanislaveleninweb@gmail.com>
2020-06-04 15:30:03 +0300

    Commit

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
@@ -20,6 +22,8 @@ class AdminController extends Controller
         $data['mostViewed'] = [];
         $data['recentPosts'] = [];
         $data['recentComments'] = [];
+        
+        Mail::to(Auth::user())->send(new \App\Mail\OrderNotShipped());
         
         return view('home.admin.index')->with($data);
     }
